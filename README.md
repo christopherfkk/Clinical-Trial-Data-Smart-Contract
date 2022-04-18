f4696cc702d660df378aaf2ad39f0ec5b418ff13
Christopher Fok <66243575+christopherfkk@users.noreply.github.com>
2022-04-18 21:01:55 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,65 @@
+## Clinical Trial Data Use Case
+This use case secures the data progression of clinical trial data, using Phala Network's Fat Contract framework to deploy an ink! smart contract. 
+
+## Overview
+Clinical trials are conducted in more decentralized manners because 1) patient mobility is restricted during COVID-19, and 2) a higher patient diversity is demanded to increase the generalizability of results. 
+
+The vision of the use case is to support virtual clinical trials, which collect patient data from multiple geographical locations, and secure its data upload, data aggregation, and significance testing processes on-chain. Read more on the proposal [here](./docs/proposal.pdf).
+
+## Architecture and product
+![](./docs/architecture.jpg)
+### Current state:
+1. **The contract can only handle up to 30 patient records [_engineering_]**. 
+    <br><br>
+    - Working with data and statistics often involves floating-point numbers and some form of pseudo-random sampling under the hood. However, their non-deterministic nature renders them incompatible, since each node in the network needs to be able to independently execute the smart contract and obtain an identitical output, to preserve the integrity of the state of the virtual machine. Consequently, decimal numbers are treated as scaled integers to preserve a fixed number of significant figures, which are de-scaled in the frontend in an unsecured manner. The current statiscal results are also analytically calculated rather than simulated, which involves larger quantities of factorials that risks integer overflow. Hence, 30 patient records is the maximum that does not cause an overflow in the u128 data type. 
+    <br><br>
+2. **The contract is currently single-use [_user experience_].** 
+    <br><br>
+    - This means it requires a researcher to build, upload, and instantiate the ink contract on his own. However, such process and responsibility should really be carried out by the engineers. It is not ideal because 1) it requires the user extra effort and literacy of contract deployment, and 2) it duplicates the contract code and occupies redundant storage on-chain. 
+    <br><br>
+3. **The statistical result is not credibly displayed [_user experience_].** 
+    <br><br>
+    - Currently, the statistical significance of the data is displayed as a pop-up on the frontend and requires the researcher to do some form of copy-and-paste on the frontend to a journal article, pharmaceutical company website, company report, et cetera. 
+    <br><br>
+
+### Goal state:
+1. **Use a Rust crate that works with statistics and probability distributions and implements a `no_std` version.** 
+    <br><br>
+    - Currently, the go-to Rust library `statrs` crate uses many floating-point data types and random processes. This is the most scalable way to include more types of statistical tests into our contract. We can either wait for the `no_std ` release of the `statrs` crate, or try revamp the library on our own by first replacing all floating points with the `fixed-point` data type, a pseudo-floating-point that is deterministic at compile time.
+    <br><br>
+2. **Implement account verification on the backend to handle multiple clinical trial datasets on a single contract.** 
+    <br><br>
+    - With this, the current storage of the full dataset needs to reduce to such as the storage of the hash of the dataset, where the full dataset resides in an external decentralized storage like Arweave. This should be similar to use cases that handle accounts of cryptocurrencies or NFTs, where anagously a single dataset is akin to a single NFT.
+    <br><br>
+3. **More investigation is needed on how best to communicate credibly the result to the desired audience.** 
+    <br><br>
+    - One way to is to make use of Phala Network Fat Contract's HTTP capabilities to sent statistical results directly to the desired website, such as the FDA's homepage or the pharceutical's blog. 
+    <br><br>
+
+## Interact with the contract 
+1. Download the code in your desired directory
+  ```
+  git clone <url>
+  ```
+2. Build the contract in the root directory. The WebAssembly `.wasm` file will appear in `\target\ink`
+  ```
+  cargo build --release
+  ```
+3. Upload the WebAssembly code to Phala Network's PoC test net on the Polkadot/Subtrate portal following this [tutorial](https://www.youtube.com/watch?v=aZGj4FhkY6A&t=3135s&ab_channel=ParityTechnologies). You should obtain your contract ID
+4. Build the Next.js frontend in the root directory
+  ```
+  npm install
+  npm build
+  ```
+5. Run the frontend and input the contract ID and endpoints 
+  ```
+  npm start
+  ```
+6. Interact!
+
+## External resources
+
+- [Set up Polkadot development accounts](https://wiki.phala.network/en-us/general/applications/01-polkadot-extension/#create-new-account)
+- [Fat Contract tutorial](https://wiki.phala.network/en-us/build/developer/fat-contract-tutorial/)
+- [Run a local Phala Blockchain test net tutorial](https://wiki.phala.network/en-us/build/developer/run-a-local-development-network/)
+
---
+++ lib.rs
@@ -0,0 +1,372 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use ink_lang as ink;
+
+#[ink::contract]
+mod clinical_trial_data {
+
+    // import ink data structures
+    use ink_prelude::string::String; 
+    use ink_prelude::vec::Vec;
+    use ink_prelude::vec; 
+    use ink_storage::Mapping;
+    use ink_storage::traits::SpreadAllocate;
+
+    #[ink(storage)]
+    #[derive(SpreadAllocate)]
+    pub struct ClinicalTrialData {
+
+        // data
+        raw_records: Vec<(String, String, String)>, // [("1", "Treatment", "Positive"), ("2", "Placebo", "Negative"), ...]
+        preprocessed_records: Vec<(String, String, String)>, // [("1", "Treatment", "Positive"), ("2", "Placebo", "Negative"), ...]
+        data_summary: Mapping<String, u128>, // {'Treatment Positive': 3, 'Treatment Negative': 385, 'Placebo Positive': 28, 'Placebo Negative': 358}
+
+        // study characteristics
+        p_thresh: u128, // p-value threshold
+        stat_test: String, // e.g. fishers_exact_test
+        p_value: Vec<u128> // [nominator, denominator], compute resulting p-value in frontend = nominator/denominator
+        result: bool, // true for significant result, i.e. p_value < p_thresh, otherwise false
+          
+    }
+
+    impl ClinicalTrialData {
+
+        // creates a new ClinicalTrialData contract initialized to the given values (done on polkadot/subtrate UI)
+        #[ink(constructor)] 
+        pub fn new(custom_p_thresh: u128, custom_stat_test: String) -> Self {
+
+            // to initialize ink_storage::Mapping
+            ink_lang::utils::initialize_contract(|contract: &mut Self| {
+                contract.p_thresh = custom_p_thresh;
+                contract.stat_test = custom_stat_test;
+            })
+        }
+
+        // creates a new ClinicalTrialData contract initialized to default values (done on polkadot/subtrate UI)
+        #[ink(constructor)]
+        pub fn default() -> Self {
+
+            // to initialize ink_storage::Mapping
+            ink_lang::utils::initialize_contract(|contract: &mut Self| {
+                contract.result = false;
+                contract.p_thresh = 5;
+                contract.stat_test = String::from("fishers_exact_test");
+            })
+        }
+
+        // sets the p-value of the ClinicalTrialData contract
+        #[ink(message)]
+        pub fn set_p_thresh(&mut self, p: u128) {
+            self.p_thresh = p;
+        }
+
+        // gets the stat test of the ClinicalTrialData contract
+        #[ink(message)]
+        pub fn set_stat_test(&mut self, stat_test: String) {
+            self.stat_test = stat_test;
+        }
+
+        // gets the p-threshold of the ClinicalTrialData contract
+        #[ink(message)]
+        pub fn get_p_thresh(&self) -> u128 {
+            self.p_thresh
+        }
+
+        // gets the stat test of the ClinicalTrialData contract
+        #[ink(message)]
+        pub fn get_stat_test(&self) -> String {
+            self.stat_test.clone()
+        }
+
+        // uploads raw records to contract storage from frontend with a whole array, use createApiType in frontend
+        #[ink(message)]
+        pub fn upload_all_raw(&mut self, records: Vec<(String, String, String)>) {
+            for record in records {
+                self.raw_records.push(record);
+            }
+        }
+
+        // uploads raw records to contract storage from frontend one-by-one
+        #[ink(message)]
+        pub fn upload_one_raw(&mut self, patient_id: String, group: String, outcome: String) {
+
+            let record: (String, String, String) = (patient_id, group, outcome);
+            self.raw_records.push(record);
+        }
+
+        // returns raw records from contract storage to frontend
+        #[ink(message)]
+        pub fn download_raw(&self) -> Vec<(String, String, String)>{
+            self.raw_records.clone()
+        }
+
+        // returns preprocessed records from contract storage to frontend
+        #[ink(message)]
+        pub fn download_preprocessed(&self) -> Vec<(String, String, String)>{
+            self.preprocessed_records.clone()
+        }
+
+        // clears raw records in contract storage
+        #[ink(message)]
+        pub fn clear_raw(&mut self) {
+            self.raw_records.clear();
+        }
+
+        // clears preprocessed records in contract storage
+        #[ink(message)]
+        pub fn clear_preprocessed(&mut self) {
+            self.preprocessed_records.clear();
+        }
+
+        // uploads preprocessed record to contract storage from frontend and returns stat test results 
+        #[ink(message)]
+        pub fn upload_all_preprocessed(&mut self, records: Vec<(String, String, String)>) {
+            for record in records {
+                self.preprocessed_records.push(record);
+            }
+            self.aggregate_data();
+            self.run_stat_test();
+        }
+
+        // uploads preprocessed record to contract storage from frontend and returns stat test results 
+        #[ink(message)]
+        pub fn upload_one_preprocessed(&mut self, patient_id: String, group: String, outcome: String) {
+            let record: (String, String, String) = (patient_id, group, outcome);
+            self.preprocessed_records.push(record);
+        }
+
+        // runs test after upload_one_proprocessed is done
+        #[ink(message)]
+        pub fn run_on_preprocessed(&mut self) {
+            self.aggregate_data();
+            self.run_stat_test();
+        }
+
+        // gets result
+        #[ink(message)]
+        pub fn get_result(&self) -> bool {
+            self.result
+        }
+
+        // gets p-value
+        #[ink(message)]
+        pub fn get_p_value(&self) -> Vec<u128> {
+            self.p_value.clone()
+        }
+
+        // aggregates preprocessed records to data summary (access: owner)
+        pub fn aggregate_data(&mut self) {
+
+            // 1. initiazlie variables
+            let mut treatment_pos: u128 = 0;
+            let mut treatment_neg: u128 = 0;
+            let mut placebo_pos: u128 = 0;
+            let mut placebo_neg: u128 = 0;
+
+            // 2. iterate through preprocessed records
+            for patient in self.preprocessed_records.iter() {
+
+                if patient.1 == "Treatment" {
+                    if patient.2 == "Yes" {
+                        treatment_pos += 1;
+                    } else {
+                        treatment_neg += 1;
+                    }
+                } else {
+                    if patient.2 == "Yes" {
+                        placebo_pos += 1; 
+                    } else {
+                        placebo_neg += 1;
+                    }
+                } 
+            }
+
+            // 3. insert into Mapping into contract storage
+            self.data_summary.insert(String::from("Treatment Positive"), &treatment_pos);
+            self.data_summary.insert(String::from("Treatment Negative"), &treatment_neg);
+            self.data_summary.insert(String::from("Placebo Positive"), &placebo_pos);
+            self.data_summary.insert(String::from("Placebo Negative"), &placebo_neg);
+        }
+
+        // calculates factorial iteratively
+        pub fn factorial(&self, num: u128) -> u128 {
+            if num == 0 {
+                1
+            }
+            else{
+                (1..=num).fold(1, |acc, v| acc * v)
+            }
+        }
+        
+        // calculates fisher's exact test formulaically
+        pub fn binomial(&self, val1: u128, val2: u128) -> u128{
+            self.factorial(val1) / (self.factorial(val2) * self.factorial(&val1 - &val2))
+        }
+
+        // calculates p-value using hypergeometric distribution in fisher's exact test
+        pub fn hypergeom_cdf(&self, population: u128, cured: u128, treatment: u128, observed: u128) -> u128 {
+            let mut hypergeom_sum: u128 = 0;
+            while observed <= cured && observed <= treatment{
+                hypergeom_sum += self.binomial(cured, observed) * self.binomial(population-cured, treatment-observed);
+                observed += 1;
+            }
+            hypergeom_sum
+        }
+
+        // runs statistical test on data summary 
+        pub fn run_stat_test(&mut self) {
+
+            // 1. read self.data_summary
+            let treatment_pos = self.data_summary.get(String::from("Treatment Positive")).unwrap();
+            let treatment_neg = self.data_summary.get(String::from("Treatment Negative")).unwrap();
+            let placebo_pos = self.data_summary.get(String::from("Treatment Positive")).unwrap();
+            let placebo_neg = self.data_summary.get(String::from("Treatment Negative")).unwrap();
+            
+            // 2. get hypergeomtric parameters
+            let population  = treatment_pos + treatment_neg + placebo_pos + placebo_neg;
+            let cured = treatment_pos + placebo_pos;
+            let treatment = treatment_pos + treatment_neg;
+            let observed = treatment_pos; 
+
+            // 3. significant figure multiplier
+            let scalar: u128 = 100; // significant figure multiplier
+            let scaled_p: u128 = self.p_thresh * self.binomial(population, treatment); 
+            let mut scaled_right_cdf: u128 = self.hypergeom_cdf(population, cured, treatment, observed);
+            if scaled_right_cdf > self.binomial(population, treatment)/2 {
+                scaled_right_cdf = self.binomial(population, treatment) - scaled_right_cdf;
+            }
+            // 4. compare p-value with p-thresh
+            self.p_value = vec![scaled_right_cdf, self.binomial(population, treatment)];
+            if scaled_right_cdf*scalar < scaled_p {
+                self.result = true;
+            }
+        }
+    }
+
+    // off-chain unit tests for ClinicalTrialData contract
+    #[cfg(test)]
+    mod tests {
+        
+        use super::*; // imports all definitions from the outer scope
+        use ink_lang as ink; // imports `ink_lang` so we can use `#[ink::test]`
+        use ink_prelude::string::ToString;
+
+        #[ink::test]
+        fn init_works() {
+            let research = ClinicalTrialData::default();
+            assert!(research.get_p_thresh() == 5 && research.get_stat_test() == String::from("fishers_exact_test"));
+
+            let research = ClinicalTrialData::new(2, String::from("t_test"));
+            assert!(research.get_p_thresh() == 2 && research.get_stat_test() == String::from("t_test"));
+        }
+
+        #[ink::test]
+        fn upload_all_works() {
+            
+            let sample: Vec<(String, String, String)> = vec![
+                ("1", "Treatment", "Yes"), ("2", "Treatment", "Yes"), ("3", "Treatment", "Yes"), 
+                ("4", "Treatment", "Yes"), ("5", "Treatment", "No"), ("6", "Treatment", "No"), 
+                ("7", "Treatment", "No"), ("8", "Placebo", "Yes"), ("9", "Placebo", "Yes"), 
+                ("10", "Placebo", "Yes"),("111", "Placebo", "Yes"), ("112", "Placebo", "Yes"), 
+                ("113", "Placebo", "Yes"), ("114", "Placebo", "Yes"), ("115", "Placebo", "Yes"),
+                ("431", "Placebo", "Yes"), ("432", "Placebo", "Yes"), ("433", "Placebo", "No"), 
+                ("434", "Placebo", "No"), ("435", "Placebo", "No"), ("436", "Placebo", "No"), 
+                ("440", "Placebo", "No"), ("23", "Treatment", "Yes"), ("24", "Treatment", "Yes"), 
+                ("25", "Treatment", "Yes"), ("26", "Treatment", "Yes"), ("27", "Treatment", "Yes"), 
+                ("28", "Treatment", "Yes"), ("29", "Treatment", "Yes"), ("30", "Treatment", "Yes"), 
+                ("31", "Treatment", "Yes"), ("45", "Treatment", "Yes"),("85", "Treatment", "Yes")]
+                    .iter()
+                    .map(|x| (x.0.to_string(), x.1.to_string(), x.2.to_string()))
+                    .collect::<Vec<(String, String, String)>>();
+            
+            // initialize default contract with p = 0.05 and fisher's exact test
+            let mut research = ClinicalTrialData::default();
+
+            // test raw records upload
+            research.upload_all_raw(sample.clone());
+            assert!(research.raw_records == sample);
+
+            // test raw records download
+            let download = research.download_raw();
+            assert!(download == research.raw_records);
+
+            // clear raw records
+            research.clear_raw();
+            assert!(research.raw_records.len() == 0);
+
+            // test preprocessed records upload 
+            research.upload_all_preprocessed(sample.clone());
+            assert!(research.preprocessed_records == sample);
+
+            // test data aggregation
+            assert!(research.data_summary.get(&String::from("Treatment Positive")).unwrap() == 15);
+            assert!(research.data_summary.get(&String::from("Treatment Negative")).unwrap() == 3);
+            assert!(research.data_summary.get(&String::from("Placebo Positive")).unwrap() == 10);
+            assert!(research.data_summary.get(&String::from("Placebo Negative")).unwrap() == 5);
+            
+            // test statistical test
+            ink_env::debug_println!("p-value: {:?}", research.p_value);
+            assert!(research.result == true);
+        }
+
+        #[ink::test]
+        fn upload_one_by_one_works() {
+            
+            let sample: Vec<(String, String, String)> = vec![
+                ("1", "Treatment", "Yes"), ("2", "Treatment", "Yes"), ("3", "Treatment", "Yes"), 
+                ("4", "Treatment", "Yes"), ("5", "Treatment", "No"), ("6", "Treatment", "No"), 
+                ("7", "Treatment", "No"), ("8", "Placebo", "Yes"), ("9", "Placebo", "Yes"), 
+                ("10", "Placebo", "Yes"),("111", "Placebo", "Yes"), ("112", "Placebo", "Yes"), 
+                ("113", "Placebo", "Yes"), ("114", "Placebo", "Yes"), ("115", "Placebo", "Yes"),
+                ("431", "Placebo", "Yes"), ("432", "Placebo", "Yes"), ("433", "Placebo", "No"), 
+                ("434", "Placebo", "No"), ("435", "Placebo", "No"), ("436", "Placebo", "No"), 
+                ("440", "Placebo", "No"), ("23", "Treatment", "Yes"), ("24", "Treatment", "Yes"), 
+                ("25", "Treatment", "Yes"), ("26", "Treatment", "Yes"), ("27", "Treatment", "Yes"), 
+                ("28", "Treatment", "Yes"), ("29", "Treatment", "Yes"), ("30", "Treatment", "Yes"), 
+                ("31", "Treatment", "Yes"), ("45", "Treatment", "Yes"),("85", "Treatment", "Yes")]
+                    .iter()
+                    .map(|x| (x.0.to_string(), x.1.to_string(), x.2.to_string()))
+                    .collect::<Vec<(String, String, String)>>();
+            
+            // initialize default contract with p = 0.05 and fisher's exact test
+            let mut research = ClinicalTrialData::default();
+
+            // set p-value and stat test
+            research.set_p_thresh(6);
+            research.set_stat_test(String::from("difference_of_means_test"));
+            assert!(research.p_thresh == 6);
+            assert!(research.stat_test == "difference_of_means_test");
+            
+            // revert back to default since contract doesn't have difference of means yet
+            research.set_p_thresh(5);
+            research.set_stat_test(String::from("fishers_exact_test"));
+
+            // test raw records upload one by one
+            for patient in sample.clone().iter() {
+                research.upload_one_raw(patient.0.clone(), patient.1.clone(), patient.2.clone())
+            }
+            assert!(research.raw_records == sample);
+
+            // test raw records download
+            let download = research.download_raw();
+            assert!(download == research.raw_records);
+
+            // test preprocessed records upload 
+            for patient in sample.clone().iter() {
+                research.upload_one_preprocessed(patient.0.clone(), patient.1.clone(), patient.2.clone())
+            };
+            research.run_on_preprocessed();
+            assert!(research.preprocessed_records == sample);
+
+            // test data aggregation
+            assert!(research.data_summary.get(&String::from("Treatment Positive")).unwrap() == 15);
+            assert!(research.data_summary.get(&String::from("Treatment Negative")).unwrap() == 3);
+            assert!(research.data_summary.get(&String::from("Placebo Positive")).unwrap() == 10);
+            assert!(research.data_summary.get(&String::from("Placebo Negative")).unwrap() == 5);
+            
+            // test statistical test
+            ink_env::debug_println!("p-value: {:?}", research.p_value);
+            assert!(research.result == true);
+        }
+    }
+}
